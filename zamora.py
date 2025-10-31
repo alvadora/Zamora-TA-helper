b0e30a2b6f1239d15e328cc22d524072625f2ac8
alvadora <73154260+alvadora@users.noreply.github.com>
2025-10-31 20:42:24 +0800

    zamora.py

--- zamora.py
+++ zamora.py
@@ -420,6 +420,7 @@ def get_crafting_tree_string(item, qty, indent="", is_last=True):
 
 def TA_page():
     st.title('TA helper')
+    st.markdown("<h3 style = 'text-align: center; color: white; ">TA Material Calculator</h3>", unsafe_allow_html = True)
     st.divider()
     col1, col2 = st.columns([3,2], vertical_alignment = 'bottom')
 
@@ -455,3 +456,4 @@ pages = {'TA helper':[
 
 pg = st.navigation(pages)
 pg.run()
+
